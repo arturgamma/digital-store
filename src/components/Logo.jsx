90440ea9b3b1d018c0bc3075c68f51cb0b189279
arturgamma <arturgamma@gmail.com>
2024-10-19 10:55:55 -0300

    início de implementação do layout(header e foor)

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+    return ( 
+        <div>
+            <img src="../assets/logo-header.svg" alt="Digital College" />
+        </div>
+     );
+        
+}
+ 
+export default Logo;
